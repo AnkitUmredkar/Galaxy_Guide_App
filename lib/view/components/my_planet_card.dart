762d275671ef18ff338f892de75baf7e1cc7a6b8
Ankit <ankitumredkar77@gmail.com>
2024-12-02 22:03:16 +0530

    Initial commit

--- lib/view/components/my_planet_card.dart
+++ lib/view/components/my_planet_card.dart
@@ -63,6 +63,8 @@ class MyPlanetCard extends StatelessWidget {
                       shape: BoxShape.circle,
                     ),
                     child: Hero(
+
+
                       tag: "Planet $index",
                       child: ModelViewer(
                         autoRotate: true,
